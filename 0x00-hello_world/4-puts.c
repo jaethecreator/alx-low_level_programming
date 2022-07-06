bf278d6c43f73e02392e64df1f8491a704ed17cb
jaethecreator <jidekelekun@gmail.com>
2022-07-05 18:27:52 -0700

    new 11

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main entry 
  * *random commentss to please yall
+ * 
+ * return = always 0 (succes)
  */
-
 int main(void)
 {
 char *s = "\"Programming is like building a multilingual puzzle";
