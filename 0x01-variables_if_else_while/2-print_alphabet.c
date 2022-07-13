355bc6b4525106189f48fb7284998c960f916568
jaethecreator <jidekelekun@gmail.com>
2022-07-12 17:35:13 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return 0 on success
+ */
+
+int main(void)
+
+{
+	char ch = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(ch);
+
+	
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return 0 on success
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+		putchar('\n');
+
+	return (0);
+}
